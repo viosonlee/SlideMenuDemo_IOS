d01678fb1152584285309c977b6977676c994889
lifeng <lifeng@lifengdemac.mshome.net>
2016-08-23 16:35:18 +0800

    Initial Commit

---
+++ SlideMenuDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuDemo
+//
+//  Created by lifeng on 16/8/23.
+//  Copyright © 2016年 lifeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
